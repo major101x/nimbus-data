804eb54273211f055a980f33821a54349f151e80
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2026-07-05 18:49:33 +0100

    implement error handling and mock weather event

--- producer/src/main.rs
+++ producer/src/main.rs
@@ -4,13 +4,33 @@ use rdkafka::{
     producer::{FutureProducer, FutureRecord},
     util::Timeout,
 };
+use serde::{Deserialize, Serialize};
 use std::{env, time::Duration};
 use tokio::time::sleep;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct WeatherEvent {
+    station_id: String,
+    temperature: f32,
+    humidity: f32,
+    wind_speed: f32,
+    timestamp: String,
+}
+
 async fn produce(brokers: &str, topic: &str) {
+    let weather_event = WeatherEvent {
+        station_id: String::from("station-123"),
+        temperature: 29.3,
+        humidity: 60.1,
+        wind_speed: 12.4,
+        timestamp: String::from("2026-06-18T12:00:00Z"),
+    };
+
+    let serialized_event = serde_json::to_string(&weather_event).unwrap();
+
     let producer: &FutureProducer = &ClientConfig::new()
-        .set("bootstrap.server", brokers)
-        .set("message.timeout.ms", "5000")
+        .set("bootstrap.servers", brokers)
+        .set("message.timeout.ms", "50000")
         .create()
         .expect("Producer creation error");
 
@@ -18,10 +38,15 @@ async fn produce(brokers: &str, topic: &str) {
     loop {
         let key_str = format!("{}", i);
 
-        let _ = producer.send(
-            FutureRecord::to(topic).payload("Hello").key(&key_str),
+        let delivery_status = producer.send(
+            FutureRecord::to(topic).payload(&serialized_event).key(&weather_event.station_id),
             Timeout::After(Duration::from_mins(1)),
-        );
+        ).await;
+
+        match delivery_status {
+            Ok(m) => println!("Message sent! Partition: {}, Offset: {}, Timestamp: {:?}", m.partition, m.offset, m.timestamp),
+            Err(e) => print!("Message not sent! Payload: {:#?}", e)
+        }
 
         println!("Delivery status for message {} received", i);
 
@@ -30,7 +55,7 @@ async fn produce(brokers: &str, topic: &str) {
     }
 }
 
-async fn loopfn () {
+async fn loopfn() {
     let mut i = 0;
     loop {
         println!("Delivery status for message {} sent", i);
@@ -43,8 +68,9 @@ async fn loopfn () {
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    let kafka_cluster_url = env::var("KAFKA_CLUSTER_URL");
-    let kafka_topic = env::var("KAFKA_TOPIC");
+    let kafka_cluster_url = env::var("KAFKA_CLUSTER_URL").unwrap();
+    let kafka_topic = env::var("KAFKA_TOPIC").unwrap();
 
-    loopfn().await;
+    // loopfn().await;
+    produce(kafka_cluster_url.as_str(), kafka_topic.as_str()).await
 }
