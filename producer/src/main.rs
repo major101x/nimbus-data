4cb5eea3cd0f7e6a74d357bd6bb4d605da09634d
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2026-07-09 16:00:53 +0100

    Remove hardcoded weather events and replace with random weather generation function

--- producer/src/main.rs
+++ producer/src/main.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use rand::RngExt;
 use rdkafka::{
     config::ClientConfig,
     producer::{FutureProducer, FutureRecord},
@@ -17,17 +18,25 @@ struct WeatherEvent {
     timestamp: String,
 }
 
+fn generate_weather_event(i: u64) -> WeatherEvent {
+    let mut rng = rand::rng();
+    
+    let station_id = format!("IST-{}", i % 10);
+    let temperature: f32 = rng.random_range(-50.0..60.0);
+    let humidity:f32 = rng.random_range(0.0..100.0);
+    let wind_speed: f32 = rng.random_range(0.00..75.0);
+    let timestamp = String::from("2026-06-18T12:00:00Z");
+
+    WeatherEvent {
+        station_id,
+        temperature,
+        humidity,
+        wind_speed,
+        timestamp
+    }
+}
+
 async fn produce(brokers: &str, topic: &str) {
-    let weather_event = WeatherEvent {
-        station_id: String::from("station-123"),
-        temperature: 29.3,
-        humidity: 60.1,
-        wind_speed: 12.4,
-        timestamp: String::from("2026-06-18T12:00:00Z"),
-    };
-
-    let serialized_event = serde_json::to_string(&weather_event).unwrap();
-
     let producer: &FutureProducer = &ClientConfig::new()
         .set("bootstrap.servers", brokers)
         .set("message.timeout.ms", "50000")
@@ -36,6 +45,9 @@ async fn produce(brokers: &str, topic: &str) {
 
     let mut i = 0;
     loop {
+        let weather_event = generate_weather_event(i);
+        let serialized_event = serde_json::to_string(&weather_event).unwrap();
+
         let delivery_status = producer.send(
             FutureRecord::to(topic).payload(&serialized_event).key(&weather_event.station_id),
             Timeout::After(Duration::from_mins(1)),
@@ -53,22 +65,11 @@ async fn produce(brokers: &str, topic: &str) {
     }
 }
 
-async fn loopfn() {
-    let mut i = 0;
-    loop {
-        println!("Delivery status for message {} sent", i);
-
-        i += 1;
-        sleep(Duration::from_millis(100)).await;
-    }
-}
-
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     let kafka_cluster_url = env::var("KAFKA_CLUSTER_URL").unwrap();
     let kafka_topic = env::var("KAFKA_TOPIC").unwrap();
 
-    // loopfn().await;
     produce(kafka_cluster_url.as_str(), kafka_topic.as_str()).await
 }
